55a4780faf270d4258a1edfcde542dddc24b899c
EseyomaFelix <feseyoma@gmail.com>
2022-07-05 22:05:38 -0700

    ~a program that prints _putchar, followed by a new line.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,20 @@
-a program that prints _putchar, followed by a new line.
+#include "main.h"
+
+/**
+ * main-Prints"main.h"fpllowwd by a new line.
+ * Return:Always 0.
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
