9775118fc8cb6ea82499528765a43ee2229ef51c
EseyomaFelix <feseyoma@gmail.com>
2022-07-06 03:54:31 -0700

    may

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,6 +6,7 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
 	unsigned long fib1 = 0, fib2 = 1, fibsum;
@@ -16,13 +17,11 @@ int main(void)
 	
 	fibsum = fib1 + fib2;
 	if (fibsum > 4000000)
-		break;
-	
+	break;
 	if ((fibsum % 2) == 0)
-		tot_sum += fibsum;
-	
-		fib1 = fib2;
-		fib2 = fibsum;
+	tot_sum += fibsum;
+	fib1 = fib2;
+	fib2 = fibsum;
 	}
 	printf("%.0f\n", tot_sum);
 	return (0);
