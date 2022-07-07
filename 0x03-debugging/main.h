d022f78c6f5aef85df2f532100fcf794e71511c8
EseyomaFelix <feseyoma@gmail.com>
2022-07-07 06:26:52 -0700

    first task

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,5 +4,8 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-
+int positive_number(int a, int b, int
+void print_remainig_days(int month,
+int convert_day(int month, int day);
+void print_times_table(int n);
 #endif /* MAIN_H */
